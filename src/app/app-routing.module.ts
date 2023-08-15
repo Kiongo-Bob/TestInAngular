d966d8ac7b7a8fe1eaef10af8fa00cc675f13c58
Kiongo-Bob <njoguub@gmail.com>
2023-08-15 17:00:56 +0300

    Testing Angular apps init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path: 'register', component:RegisterComponent},
+  {path: 'navbar', component:NavbarComponent},
+  {path: 'about', component:AboutComponent}
+]; //Where you create your routes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
